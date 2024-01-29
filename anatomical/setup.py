c10f00326fb7142e0dae2d61142483a154423601
MarieStLaurent <marie.stl@gmail.com>
2024-01-29 14:57:34 -0500

    setting up

---
+++ anatomical/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Data, scripts and derrivatives for the CNeuroMod-THINGS dataset',
+    author='CNeuroMod and ViCCo',
+    license='MIT',
+)
