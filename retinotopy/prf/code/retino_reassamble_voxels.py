bf08e3ccd4e443d2315cccc0f9244d0366cbe4ee
MarieStLaurent <marie.stl@gmail.com>
2024-06-10 15:04:05 -0400

    reassamble vox

--- retinotopy/prf/code/retino_reassamble_voxels.py
+++ retinotopy/prf/code/retino_reassamble_voxels.py
@@ -45,7 +45,7 @@ def reassamble(dir_path, sub, mask, num_vox, chunk_size, out):
     )
 
     for i in range(int(np.ceil(num_vox/chunk_size))):
-        flat_output[i*chunk_size:(i+1)*chunk_size] = loadmat(file_path[i])[out].reshape(-1,)
+        flat_output[i*chunk_size:np.min(num_vox, (i+1)*chunk_size)] = loadmat(file_path[i])[out].reshape(-1,)
 
     savemat(
         f"{dir_path}/sub-{sub}/output/sub-{sub}_task-retinotopy_space-T1w_"
